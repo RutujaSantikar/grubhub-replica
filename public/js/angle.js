5ebedb712cd4186f84c8ff1d8ddd9541b4cc3a25
Rutuja Santikar <santikarrutuja@gmail.com>
2022-09-25 14:26:20 +0530

    commit

--- public/js/angle.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const dropDownBtn = document.getElementById("show-content-1");
-dropDownBtn.addEventListener("click", function () {
-    console.log("clickedd");
-
-  document.getElementById("show-content-2").classList.toggle("show");
-});
